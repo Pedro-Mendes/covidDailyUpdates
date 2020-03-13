42430b106bb2f4ab896d9ed90f0d9cff7c438634
Pedro Mendes <pedro.mendes.unicamp@gmail.com>
2020-03-12 23:28:59 -0300

    Adding function to tweet the current time every 10 seconds

--- bot.js
+++ bot.js
@@ -4,4 +4,23 @@ dotenv.config();
 const Twit = require('twit');
 const config = require('./config');
 
-let T = new Twit(config);
+const T = new Twit(config);
+
+function tweeted(err, data, response) {
+  if (err) {
+    console.log('An error occurred: ', response);
+  } else {
+    console.log('All good');
+  }
+}
+function scheduledTweet() {
+  const date = new Date(); // for now
+  const dateString = `Agora são ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const tweet = {
+    status: dateString,
+  };
+  T.post('statuses/update', tweet, tweeted);
+}
+
+scheduledTweet();
+setInterval(scheduledTweet, 1000 * 10);
