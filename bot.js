afb9e8d19679e5d6a2fd3698f4c556886e476b23
Pedro <pedro.mendes.unicamp@gmail.com>
2020-03-19 00:07:56 -0300

    Incrementing heroku integration with covid-19 API

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
+/* https://rapidapi.com/KishCom/api/covid-19-coronavirus-statistics */
 const dotenv = require('dotenv');
+const axios = require('axios');
 
 dotenv.config();
 const Twit = require('twit');
@@ -13,14 +15,37 @@ function tweeted(err, data, response) {
     console.log('All good');
   }
 }
+
+function getCorona() {
+  return axios({
+    method: 'GET',
+    url: 'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats',
+    headers: {
+      'content-type': 'application/octet-stream',
+      'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
+      'x-rapidapi-key': 'f3aaada3fdmsh01752c4dfa674acp12ea76jsnfe30e4023c44',
+    },
+    params: {
+      country: 'Brazil',
+    },
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log('Error with response: ', error);
+    });
+}
 function scheduledTweet() {
-  const date = new Date(); // for now
-  const dateString = `Agora são ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  const tweet = {
-    status: dateString,
-  };
-  T.post('statuses/update', tweet, tweeted);
+  const date = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+  let confirmedCases;
+  getCorona()
+    .then((res) => {
+      confirmedCases = res.data.covid19Stats[0].confirmed;
+      const tweet = {
+        status: `${date}\n Brazil confirmed cases: ${confirmedCases}`,
+      };
+      T.post('statuses/update', tweet, tweeted);
+    });
 }
 
 scheduledTweet();
-setInterval(scheduledTweet, 1000 * 10);
+setInterval(scheduledTweet, 1000 * 60 * 60);
