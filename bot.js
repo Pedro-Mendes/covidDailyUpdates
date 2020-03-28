38b08d8eb50527af905a10d07729f7eb48e70301
Pedro <pedro.mendes.unicamp@gmail.com>
2020-03-28 17:24:05 -0300

    Setting date to avoid repeating tweets statuses

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-/* https://rapidapi.com/KishCom/api/covid-19-coronavirus-statistics 
+/* https://rapidapi.com/KishCom/api/covid-19-coronavirus-statistics
 https://www.youtube.com/user/shiffman
 */
 const dotenv = require('dotenv');
@@ -53,23 +53,20 @@ function postTweet(tweet) {
 
 function scheduledTweet() {
   const date = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+  // eslint-disable-next-line spaced-comment
+  /*const date = new Date();
+  let formattedDate = new Date(date.valueOf() - date.getTimezoneOffset() * 60000);
+  formattedDate = formattedDate.toISOString().replace(/\.\d{3}Z$/, ''); */
   let confirmedCases;
   getCorona()
     .then((res) => {
       confirmedCases = res.data.covid19Stats[0].confirmed;
       const tweet = {
-        status: `${date}\n Brazil confirmed cases: ${confirmedCases}`,
+        status: `${date} Brazil confirmed cases: ${confirmedCases}`,
       };
       postTweet(tweet);
     });
 }
-
-/* function scheduleTweetLite() {
-  const T = new TwitterLite(configLite);
-  T.post('statuses/update', { status: 'Hello, Worldfd!' })
-    .then(() => console.log('Success!'))
-    .catch((err) => console.log('Error: ', err));
-} */
 
 scheduledTweet();
 setInterval(scheduledTweet, 1000 * 60 * 60 * 1);
